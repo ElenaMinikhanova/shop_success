bee1c2c5b99a814b4fb954cf7af2315534e3f12c
ElenaMinikhanova <elenaminikhanova89@gmail.com>
2025-07-16 22:11:44 +0700

    shop

--- shop/catalog/admin.py
+++ shop/catalog/admin.py
@@ -2,4 +2,7 @@ from django.contrib import admin
 from .models import Catalog
 # Register your models here.
 
-admin.site.register(Catalog)
+
+@admin.register(Catalog)
+class ProductAdmin(admin.ModelAdmin):
+    search_fields = ['category', 'name']
