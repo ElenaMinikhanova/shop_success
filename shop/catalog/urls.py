ec4877f1307664c954046c136071ff9d632cf262
ElenaMinikhanova <elenaminikhanova89@gmail.com>
2025-07-18 00:33:22 +0700

    shop

--- shop/catalog/urls.py
+++ shop/catalog/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('', PostListView.as_view(), name='view'),
     path('product/<int:pk>', PostDetailView.as_view(), name='detail'),
     path('favorites/', TemplateView.as_view(template_name='favorites.html'), name='favorites'),
+path('about_us/', TemplateView.as_view(template_name='about_us.html'), name='about_us'),
 ]
