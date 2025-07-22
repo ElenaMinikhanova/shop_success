182beb5c4e76a7deb869d3c78f77734ece7d86db
ElenaMinikhanova <elenaminikhanova89@gmail.com>
2025-07-22 23:54:22 +0700

    shop

--- shop/catalog/urls.py
+++ shop/catalog/urls.py
@@ -1,10 +1,15 @@
 from django.urls import path
 from django.views.generic import TemplateView
-from .views import PostListView, PostDetailView, RegisterView
+from .views import PostListView, PostDetailView, RegisterView, ProfileView
+from django.contrib.auth import views as auth_views
+from django.contrib.auth.views import LoginView, LogoutView
 
 urlpatterns = [
     path('', PostListView.as_view(), name='view'),
     path('product/<int:pk>', PostDetailView.as_view(), name='detail'),
     path('about_us/', TemplateView.as_view(template_name='about_us.html'), name='about_us'),
     path('register/', RegisterView.as_view(), name='register'),
+    path('exit/', LogoutView.as_view(), name='exit'),
+    path('login/', LoginView.as_view(template_name='login.html'), name='login'),
+    path('accounts/profile/', ProfileView.as_view(), name='profile'),
 ]
--- shop/catalog/views.py
+++ shop/catalog/views.py
@@ -14,6 +14,23 @@ class PostListView(ListView):
     template_name = 'base.html'
     context_object_name = 'products'
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        user = self.request.user
+
+        if user.is_authenticated:
+            # Количество лайков этого пользователя
+            context['like_count'] = user.user_likes.count()
+
+            # Количество товаров в корзине этого пользователя
+            context['basket_count'] = user.user_products.count()
+        else:
+            # Для не авторизованных можно оставить 0 или None
+            context['like_count'] = 0
+            context['basket_count'] = 0
+
+        return context
+
 class PostDetailView(DetailView):
     model = Product
     template_name = 'product.html'
@@ -33,3 +50,6 @@ class RegisterView(View):
             login(request, user)
             return redirect('view')  # или куда нужно
         return render(request, 'registration.html', {'form': form})
+
+class ProfileView(LoginRequiredMixin, TemplateView):
+    template_name = 'profile.html'
