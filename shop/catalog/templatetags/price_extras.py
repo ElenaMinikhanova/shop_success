b8d07d6c66927c580a4bb4e791e3614b3ddf3d87
ElenaMinikhanova <elenaminikhanova89@gmail.com>
2025-07-27 00:13:22 +0700

    shop

---
+++ shop/catalog/templatetags/price_extras.py
@@ -0,0 +1,27 @@
+from django import template
+
+register = template.Library()
+
+
+@register.filter
+def mul(value, arg):
+    try:
+        return float(value) * float(arg)
+    except (ValueError, TypeError):
+        return ''
+
+
+@register.filter
+def div(value, arg):
+    try:
+        return float(value) / float(arg)
+    except (ValueError, ZeroDivisionError, TypeError):
+        return ''
+
+
+@register.filter
+def subtract(value, arg):
+    try:
+        return float(value) - float(arg)
+    except (ValueError, TypeError):
+        return ''
